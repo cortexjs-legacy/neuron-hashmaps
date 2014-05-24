e277430f10c2c1e6f42a123ad56303b369f8b20c
kaelzhang <i@kael.me>
2014-05-25 05:26:33 +0800

    adds test cases

--- test/neuron-hashmaps.js
+++ test/neuron-hashmaps.js
@@ -2,3 +2,18 @@
 
 var expect = require('chai').expect;
 var hashmaps = require('../');
+var jsonfile = require('jsonfile');
+var node_path = require('path');
+
+var file = node_path.join(__dirname, 'fixtures', 'shrinkwrap.json');
+var shrinkwrap = jsonfile.readFileSync(file);
+
+describe("hashmaps(shrinkwrap)", function(){
+  var hm = hashmaps(shrinkwrap);
+  var rangeMap = hm.rangeMap();
+  var depsTree = hm.depsTree();
+
+  it("uses the first met range", function(done){
+    expect(rangeMap.c['~2.0.0']).to.equal('2.0.9');
+  });
+});
